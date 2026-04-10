0cd67db44d1d7fb6a533fd8e668d6bed2a3b00e0
juan fornieles <jfornielestiburcio02@gmail.com>
2026-04-10 13:58:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5014911262-ee6e6",
+  "appId": "1:839128312548:web:f104ae0c961aaba3d1daf1",
+  "apiKey": "AIzaSyCpQ7ra0eLj8kskc_3hxCJSlV_z8N6nPy4",
+  "authDomain": "studio-5014911262-ee6e6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "839128312548"
+};
