84b3df4687fc0415aad5a4679adbb623b9b2de15
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-04-27 15:16:45 +0200

    Update page.tsx

--- src/app/secvir/jsp/IdenUsu.jsp/page.tsx
+++ src/app/secvir/jsp/IdenUsu.jsp/page.tsx
@@ -6,10 +6,9 @@ export default function Page() {
   const [usuario, setUsuario] = useState("");
   const [clave, setClave] = useState("16829");
   const [claveCifrada, setClaveCifrada] = useState("16829");
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-    window.name = "NV_1777295521425";
+    window.name = "NV_1777295631436";
   }, []);
 
   function cifrar(texto) {
@@ -31,7 +30,6 @@ export default function Page() {
 
     setClaveCifrada(cifrar(clave));
     setClave("");
-    setError(true);
   }
 
   return (
@@ -50,44 +48,20 @@ export default function Page() {
           border-collapse: collapse;
         }
 
-        .morado {
-          background-color: #9A6289;
-        }
-
-        .blanco {
-          background-color: #FFFFFF;
-        }
-
-        .lila {
-          background-color: #BE9BB4;
-        }
-
-        .moradoclaro {
-          background-color: #DBC6D4;
-        }
-
-        .verdeagua {
-          background-color: #B7DDC8;
-        }
-
-        .verde {
-          background-color: #B3D76B;
-        }
-
-        .naranja {
-          background-color: #EAB863;
-        }
+        .morado { background-color: #9A6289; }
+        .blanco { background-color: #FFFFFF; }
+        .lila { background-color: #BE9BB4; }
+        .moradoclaro { background-color: #DBC6D4; }
+        .verdeagua { background-color: #B7DDC8; }
+        .verde { background-color: #B3D76B; }
+        .naranja { background-color: #EAB863; }
 
         .botones {
           background-color: #FFFFFF;
-          padding-top: 2px;
-          padding-right: 2px;
-          padding-bottom: 2px;
-          padding-left: 2px;
+          padding: 2px;
           border: 1px #6B4560 solid;
           font-family: Arial, Helvetica, sans-serif;
           font-size: 8pt;
-          font-weight: normal;
           color: #000000;
           width: 100px;
           text-decoration: none;
@@ -95,16 +69,7 @@ export default function Page() {
 
         .botones:hover {
           background-color: #DDCAD8;
-          padding-top: 2px;
-          padding-right: 2px;
-          padding-bottom: 2px;
-          padding-left: 2px;
-          border: 1px #6B4560 solid;
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 8pt;
-          font-weight: normal;
           color: #5A3A51;
-          text-decoration: none;
         }
 
         input {
@@ -112,19 +77,8 @@ export default function Page() {
           font-size: 8pt;
           font-weight: bold;
           color: #573957;
-          text-decoration: none;
           background-color: #DBC6D4;
-          border: #FFFFFF;
-          border-style: solid;
-          border-top-width: 1px;
-          border-right-width: 1px;
-          border-bottom-width: 1px;
-          border-left-width: 1px;
-        }
-
-        .input_check {
-          background-color: transparent;
-          border: none;
+          border: 1px solid #FFFFFF;
         }
 
         .usuario {
@@ -132,31 +86,11 @@ export default function Page() {
           font-size: 9pt;
           font-weight: bold;
           color: #FFFFFF;
-          text-decoration: none;
-        }
-
-        .usuarioMostrar {
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 9pt;
-          font-weight: bold;
-          color: #B3D76B;
-          text-decoration: none;
-        }
-
-        .mensaje {
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 12pt;
-          font-weight: bold;
-          color: #E8FF48;
-          text-decoration: none;
         }
 
         .botones2 {
           background-color: #F0AA94;
-          padding-top: 2px;
-          padding-right: 2px;
-          padding-bottom: 2px;
-          padding-left: 2px;
+          padding: 2px;
           text-align: center;
           border: 1px #E77551 solid;
           font-family: Arial, Helvetica, sans-serif;
@@ -170,96 +104,15 @@ export default function Page() {
 
         .botones2:hover {
           background-color: #B3D76B;
-          padding-top: 2px;
-          padding-right: 2px;
-          padding-bottom: 2px;
-          padding-left: 2px;
-          border: 1px solid;
-          text-align: center;
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 8pt;
-          font-weight: bold;
           color: #000000;
-          text-decoration: none;
-          border-color: #799F2B #669933 #669933;
-        }
-
-        .titulo {
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 10pt;
-          font-weight: bold;
-          color: #FFFFFF;
-          text-decoration: none;
-          background-color: #9A6289;
-          width: 100%;
-          padding-left: 10px;
-        }
-
-        .txtblanco {
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 10pt;
-          text-align: justify;
-          font-weight: normal;
-          color: #FFFFFF;
-          text-decoration: none;
-          padding-top: 4px;
-          padding-right: 4px;
-          padding-bottom: 4px;
-          padding-left: 10px;
-        }
-
-        .txtpositivo {
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 10pt;
-          font-weight: bold;
-          color: #FFFFFF;
-          text-decoration: none;
-          padding-top: 4px;
-          padding-right: 4px;
-          padding-bottom: 4px;
-          padding-left: 10px;
-        }
-
-        .txtidentificacion {
-          font-family: Arial, Helvetica, sans-serif;
-          font-size: 10pt;
-          font-weight: bold;
-          color: #DBC6D4;
-          text-decoration: none;
-          padding-top: 4px;
-          padding-right: 4px;
-          padding-bottom: 4px;
-          padding-left: 10px;
         }
 
         .txtmensajeerror {
           color: #FF9900;
           font-family: Arial, Helvetica, sans-serif;
           font-size: 10pt;
-          font-weight: normal;
           padding: 4px 4px 4px 10px;
           text-align: justify;
-          text-decoration: none;
-        }
-
-        .lista {
-          background-image: url('/secretaria_on.gif');
-          background-repeat: no-repeat;
-          background-position: 5px 5px;
-        }
-
-        .txtcabecera {
-          font-family: impact, Arial, Helvetica, sans-serif;
-          background-color: #8D5A7E;
-          color: #FFFFFF;
-          font-size: 18pt;
-        }
-
-        .blanco a {
-          color: #B3D76B;
-          font-family: tahoma;
-          font-size: 14pt;
-          text-decoration: none;
         }
       `}</style>
 
@@ -271,43 +124,51 @@ export default function Page() {
                 <tbody>
                   <tr>
                     <td>
-                      <img src="/logo_rayuela.gif" width="230" height="157" alt="" />
+                      <img
+                        src="/images/puertaTrasera/logo_rayuela.gif"
+                        width="230"
+                        height="157"
+                        alt=""
+                      />
                     </td>
-                    <td align="right" valign="bottom">&nbsp;</td>
+                    <td align="right" valign="bottom">
+                      &nbsp;
+                    </td>
                   </tr>
                 </tbody>
               </table>
             </td>
 
             <td className="morado" width="30%" valign="top">
-              <table align="center" width="90%">
+              <table
+                align="center"
+                width="90%"
+                cellPadding="0"
+                cellSpacing="5"
+              >
                 <tbody>
                   <tr>
                     <td>
-                      {error && (
-                        <div className="txtmensajeerror">
-                          El usuario, la contraseña o ambos son incorrectos, puede pedir nuevas contraseñas pulsando en la opción 'Solicitar nuevas contraseñas' o poniéndose en contacto con el C.A.U.
-                        </div>
-                      )}
-
                       <form onSubmit={comprobarclave}>
                         <table align="center">
                           <tbody>
                             <tr align="center">
-                              <td className="usuario">
+                              <td className="usuario" width="75%">
                                 Usuario
                                 <br />
                                 <input
                                   type="text"
                                   size="20"
                                   value={usuario}
-                                  onChange={(e) => setUsuario(e.target.value)}
+                                  onChange={(e) =>
+                                    setUsuario(e.target.value)
+                                  }
                                 />
                               </td>
                             </tr>
 
                             <tr align="center">
-                              <td className="usuario">
+                              <td className="usuario" width="75%">
                                 <br />
                                 Contraseña
                                 <br />
@@ -315,9 +176,15 @@ export default function Page() {
                                   type="password"
                                   size="20"
                                   value={clave}
-                                  onChange={(e) => setClave(e.target.value)}
+                                  onChange={(e) =>
+                                    setClave(e.target.value)
+                                  }
                                 />
-                                <input type="hidden" value={claveCifrada} readOnly />
+                                <input
+                                  type="hidden"
+                                  value={claveCifrada}
+                                  readOnly
+                                />
                               </td>
                             </tr>
 
@@ -325,9 +192,16 @@ export default function Page() {
                               <td>&nbsp;</td>
                             </tr>
 
+                            <tr>
+                              <td>&nbsp;</td>
+                            </tr>
+
                             <tr align="center">
-                              <td>
-                                <button type="submit" className="botones2">
+                              <td width="75%">
+                                <button
+                                  type="submit"
+                                  className="botones2"
+                                >
                                   Entrar
                                 </button>
                               </td>
@@ -341,7 +215,115 @@ export default function Page() {
               </table>
             </td>
 
-            <td width="35%">&nbsp;</td>
+            <td width="35%">
+              <table width="100%" height="151">
+                <tbody>
+                  <tr align="center">
+                    <td>&nbsp;</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+
+          <tr>
+            <td colSpan="2" className="lila" height="100" width="60%">
+              <table width="100%">
+                <tbody>
+                  <tr align="center">
+                    <td width="20%">&nbsp;</td>
+                    <td width="20%">&nbsp;</td>
+                    <td width="20%">
+                      <img
+                        src="/images/puertaTrasera/menu_GESTION_CENTROS.gif"
+                        width="103"
+                        height="69"
+                        alt=""
+                      />
+                    </td>
+                    <td width="20%">&nbsp;</td>
+                    <td width="20%">&nbsp;</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+
+            <td className="moradoclaro" align="right">
+              <img
+                src="/images/puertaTrasera/txt_ray.gif"
+                width="167"
+                height="36"
+                alt=""
+              />
+            </td>
+
+            <td className="lila" align="left">
+              <img
+                src="/images/puertaTrasera/txt_uela.gif"
+                width="166"
+                height="36"
+                alt=""
+              />
+            </td>
+          </tr>
+
+          <tr>
+            <td colSpan="2">&nbsp;</td>
+
+            <td className="verdeagua" align="right">
+              <img
+                src="/images/puertaTrasera/tit_plataforma.gif"
+                width="167"
+                height="32"
+                alt=""
+              />
+            </td>
+
+            <td className="verde" align="right">
+              <img
+                src="/images/puertaTrasera/tit_educativa.gif"
+                width="166"
+                height="32"
+                alt=""
+              />
+            </td>
+          </tr>
+
+          <tr>
+            <td colSpan="2">&nbsp;</td>
+
+            <td className="naranja">&nbsp;</td>
+
+            <td align="right">
+              <img
+                src="/images/puertaTrasera/tit_extrem.gif"
+                width="166"
+                height="17"
+                alt=""
+              />
+            </td>
+          </tr>
+
+          <tr>
+            <td colSpan="2">&nbsp;</td>
+
+            <td className="naranja">&nbsp;</td>
+
+            <td align="center">
+              <img
+                src="/images/puertaTrasera/logo_junta.gif"
+                width="166"
+                height="59"
+                alt=""
+              />
+              <br />
+              <img
+                src="/images/puertaTrasera/FEDER-UE.jpg"
+                width="165"
+                height="44"
+                alt=""
+              />
+            </td>
           </tr>
         </tbody>
       </table>
