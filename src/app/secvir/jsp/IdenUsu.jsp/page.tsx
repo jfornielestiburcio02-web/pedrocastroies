f72578ee543efb29bd308d52e5267c581210d3c9
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-04-27 15:14:56 +0200

    Update page.tsx

--- src/app/secvir/jsp/IdenUsu.jsp/page.tsx
+++ src/app/secvir/jsp/IdenUsu.jsp/page.tsx
@@ -29,11 +29,8 @@ export default function Page() {
       return;
     }
 
-    const cifrada = cifrar(clave);
-    setClaveCifrada(cifrada);
+    setClaveCifrada(cifrar(clave));
     setClave("");
-
-    // SIMULACIÓN LOGIN INCORRECTO
     setError(true);
   }
 
@@ -54,27 +51,60 @@ export default function Page() {
         }
 
         .morado {
-          background: #9a6289;
+          background-color: #9A6289;
+        }
+
+        .blanco {
+          background-color: #FFFFFF;
         }
 
         .lila {
-          background: #be9bb4;
+          background-color: #BE9BB4;
         }
 
         .moradoclaro {
-          background: #dbc6d4;
+          background-color: #DBC6D4;
         }
 
         .verdeagua {
-          background: #b7ddc8;
+          background-color: #B7DDC8;
         }
 
         .verde {
-          background: #b3d76b;
+          background-color: #B3D76B;
         }
 
         .naranja {
-          background: #eab863;
+          background-color: #EAB863;
+        }
+
+        .botones {
+          background-color: #FFFFFF;
+          padding-top: 2px;
+          padding-right: 2px;
+          padding-bottom: 2px;
+          padding-left: 2px;
+          border: 1px #6B4560 solid;
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 8pt;
+          font-weight: normal;
+          color: #000000;
+          width: 100px;
+          text-decoration: none;
+        }
+
+        .botones:hover {
+          background-color: #DDCAD8;
+          padding-top: 2px;
+          padding-right: 2px;
+          padding-bottom: 2px;
+          padding-left: 2px;
+          border: 1px #6B4560 solid;
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 8pt;
+          font-weight: normal;
+          color: #5A3A51;
+          text-decoration: none;
         }
 
         input {
@@ -82,40 +112,154 @@ export default function Page() {
           font-size: 8pt;
           font-weight: bold;
           color: #573957;
-          background: #dbc6d4;
-          border: 1px solid #fff;
+          text-decoration: none;
+          background-color: #DBC6D4;
+          border: #FFFFFF;
+          border-style: solid;
+          border-top-width: 1px;
+          border-right-width: 1px;
+          border-bottom-width: 1px;
+          border-left-width: 1px;
+        }
+
+        .input_check {
+          background-color: transparent;
+          border: none;
         }
 
         .usuario {
           font-family: Arial, Helvetica, sans-serif;
           font-size: 9pt;
           font-weight: bold;
-          color: white;
+          color: #FFFFFF;
+          text-decoration: none;
         }
 
-        .txtmensajeerror {
-          color: #ff9900;
+        .usuarioMostrar {
           font-family: Arial, Helvetica, sans-serif;
-          font-size: 10pt;
-          padding: 4px 4px 4px 10px;
-          text-align: justify;
-          margin-bottom: 10px;
+          font-size: 9pt;
+          font-weight: bold;
+          color: #B3D76B;
+          text-decoration: none;
+        }
+
+        .mensaje {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 12pt;
+          font-weight: bold;
+          color: #E8FF48;
+          text-decoration: none;
         }
 
         .botones2 {
-          background: #f0aa94;
-          border: 1px solid #e77551;
+          background-color: #F0AA94;
+          padding-top: 2px;
+          padding-right: 2px;
+          padding-bottom: 2px;
+          padding-left: 2px;
+          text-align: center;
+          border: 1px #E77551 solid;
           font-family: Arial, Helvetica, sans-serif;
           font-size: 8pt;
           font-weight: bold;
           color: #903214;
-          padding: 4px 20px;
+          width: 100px;
+          text-decoration: none;
           cursor: pointer;
         }
 
         .botones2:hover {
-          background: #b3d76b;
-          color: #000;
+          background-color: #B3D76B;
+          padding-top: 2px;
+          padding-right: 2px;
+          padding-bottom: 2px;
+          padding-left: 2px;
+          border: 1px solid;
+          text-align: center;
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 8pt;
+          font-weight: bold;
+          color: #000000;
+          text-decoration: none;
+          border-color: #799F2B #669933 #669933;
+        }
+
+        .titulo {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 10pt;
+          font-weight: bold;
+          color: #FFFFFF;
+          text-decoration: none;
+          background-color: #9A6289;
+          width: 100%;
+          padding-left: 10px;
+        }
+
+        .txtblanco {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 10pt;
+          text-align: justify;
+          font-weight: normal;
+          color: #FFFFFF;
+          text-decoration: none;
+          padding-top: 4px;
+          padding-right: 4px;
+          padding-bottom: 4px;
+          padding-left: 10px;
+        }
+
+        .txtpositivo {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 10pt;
+          font-weight: bold;
+          color: #FFFFFF;
+          text-decoration: none;
+          padding-top: 4px;
+          padding-right: 4px;
+          padding-bottom: 4px;
+          padding-left: 10px;
+        }
+
+        .txtidentificacion {
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 10pt;
+          font-weight: bold;
+          color: #DBC6D4;
+          text-decoration: none;
+          padding-top: 4px;
+          padding-right: 4px;
+          padding-bottom: 4px;
+          padding-left: 10px;
+        }
+
+        .txtmensajeerror {
+          color: #FF9900;
+          font-family: Arial, Helvetica, sans-serif;
+          font-size: 10pt;
+          font-weight: normal;
+          padding: 4px 4px 4px 10px;
+          text-align: justify;
+          text-decoration: none;
+        }
+
+        .lista {
+          background-image: url('/secretaria_on.gif');
+          background-repeat: no-repeat;
+          background-position: 5px 5px;
+        }
+
+        .txtcabecera {
+          font-family: impact, Arial, Helvetica, sans-serif;
+          background-color: #8D5A7E;
+          color: #FFFFFF;
+          font-size: 18pt;
+        }
+
+        .blanco a {
+          color: #B3D76B;
+          font-family: tahoma;
+          font-size: 14pt;
+          text-decoration: none;
         }
       `}</style>
 
@@ -127,16 +271,9 @@ export default function Page() {
                 <tbody>
                   <tr>
                     <td>
-                      <img
-                        src="/images/logo_rayuela.gif"
-                        width="230"
-                        height="157"
-                        alt=""
-                      />
+                      <img src="/logo_rayuela.gif" width="230" height="157" alt="" />
                     </td>
-                    <td align="right" valign="bottom">
-                      &nbsp;
-                    </td>
+                    <td align="right" valign="bottom">&nbsp;</td>
                   </tr>
                 </tbody>
               </table>
@@ -149,10 +286,7 @@ export default function Page() {
                     <td>
                       {error && (
                         <div className="txtmensajeerror">
-                          El usuario, la contraseña o ambos son incorrectos,
-                          puede pedir nuevas contraseñas pulsando en la opción
-                          'Solicitar nuevas contraseñas' o poniéndose en contacto
-                          con el C.A.U.
+                          El usuario, la contraseña o ambos son incorrectos, puede pedir nuevas contraseñas pulsando en la opción 'Solicitar nuevas contraseñas' o poniéndose en contacto con el C.A.U.
                         </div>
                       )}
 
@@ -164,12 +298,10 @@ export default function Page() {
                                 Usuario
                                 <br />
                                 <input
-                                  value={usuario}
-                                  onChange={(e) =>
-                                    setUsuario(e.target.value)
-                                  }
-                                  size="20"
                                   type="text"
+                                  size="20"
+                                  value={usuario}
+                                  onChange={(e) => setUsuario(e.target.value)}
                                 />
                               </td>
                             </tr>
@@ -180,18 +312,12 @@ export default function Page() {
                                 Contraseña
                                 <br />
                                 <input
-                                  value={clave}
-                                  onChange={(e) =>
-                                    setClave(e.target.value)
-                                  }
-                                  size="20"
                                   type="password"
+                                  size="20"
+                                  value={clave}
+                                  onChange={(e) => setClave(e.target.value)}
                                 />
-                                <input
-                                  type="hidden"
-                                  value={claveCifrada}
-                                  readOnly
-                                />
+                                <input type="hidden" value={claveCifrada} readOnly />
                               </td>
                             </tr>
 
@@ -201,10 +327,7 @@ export default function Page() {
 
                             <tr align="center">
                               <td>
-                                <button
-                                  type="submit"
-                                  className="botones2"
-                                >
+                                <button type="submit" className="botones2">
                                   Entrar
                                 </button>
                               </td>
@@ -219,24 +342,6 @@ export default function Page() {
             </td>
 
             <td width="35%">&nbsp;</td>
-          </tr>
-
-          <tr>
-            <td colSpan="2" className="lila" height="100"></td>
-            <td className="moradoclaro"></td>
-            <td className="lila"></td>
-          </tr>
-
-          <tr>
-            <td colSpan="2"></td>
-            <td className="verdeagua"></td>
-            <td className="verde"></td>
-          </tr>
-
-          <tr>
-            <td colSpan="2"></td>
-            <td className="naranja"></td>
-            <td></td>
           </tr>
         </tbody>
       </table>
